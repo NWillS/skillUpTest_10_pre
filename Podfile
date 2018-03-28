8729cb57074b7ac081c0e24eadaf021be71bfc45
S N <>
2018-03-28 20:28:03 +0900

    First commit

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'skillUpTest_10_pre' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for skillUpTest_10_pre
+
+  target 'skillUpTest_10_preTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ skillUpTest_10_pre/Controllers/ITunesSearch/ITunesSearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ITunesSearchViewController.swift
+//  skillUpTest_10_pre
+//
+//  Created by S N on 2018/03/28.
+//  Copyright © 2018年 Seidi Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ITunesSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
